6f84f37ea6afe039ec5f5530fa68540b6ebc80b2
kota_shiratori <swan.kouta@gmail.com>
2024-03-29 23:00:54 +0900

    🎉

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <header></header>
+    <div id="wrapper">
+        <slot />
+    </div>
+    <footer class="footer">
+        <div class="footer__inner">
+            <p class="footer__copyright">© 2022 Swan's Room</p>
+        </div>
+    </footer>
+</template>
